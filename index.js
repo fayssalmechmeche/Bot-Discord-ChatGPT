cd8153bdd3bd26ec32b59518caf5bc80e6f4fce1
fayssal <fayssalmechmeche.pro@gmail.com>
2023-08-31 20:31:02 +0200

    fix bug

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new Client({
   ],
 });
 
+let conversationLog = [];
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
@@ -17,26 +18,32 @@ client.on("ready", () => {
     activities: [{ name: "GPT Aventure !" }],
     status: "online",
   });
+  conversationLog.push({
+    role: "system",
+    content:
+      "tu es un maître de jeu de rôle. Tu vas guider un personnage à travers un monde fantastique rempli de monstres et de héros. À chaque étape, tu lui proposeras 3 choix possibles. Chaque choix a des conséquences et le joueur doit tout faire pour que son personnage survive. Le jeu se termine lorsque le personnage du joueur meurt. Dans ta première réponse, demande-lui son nom. Dans la deuxième réponse, demande-lui sa classe (chevalier, mage, archer, etc.). Dans la troisième réponse, demande-lui sa race (humain, elfe, nain, etc.). L'histoire se déroule dans un monde fantastique où un dragon terrifiant règne au sommet d'un volcan. Le but ultime du héros est de vaincre ce dragon. Prépare-toi à improviser de nombreux défis pour guider le joueur jusqu'au dragon. Une fois que le joueur a choisi sa classe et sa race, tu pourras commencer l'aventure en fonction de ses choix. Lorsque tu présenteras les choix. Tu dois prendre en compte ses réponses pour créer une expérience immersive et captivante. Si le joueur décide de mettre fin à l'aventure, tu dois absulument répondre `Fin de l'aventure !`.",
+  });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
 });
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 client.on("messageCreate", async (msg) => {
-  let conversationLog = [];
-
   if (msg.author.bot) return;
   if (msg.channel.id !== process.env.SERVEUR) return;
   if (msg.content.startsWith("!")) return;
 
-  conversationLog.push({
-    role: "system",
-    content:
-      "tu es un maître de jeu de rôle. Tu vas guider un personnage à travers un monde fantastique rempli de monstres et de héros. À chaque étape, tu lui proposeras 3 choix possibles. Chaque choix a des conséquences et le joueur doit tout faire pour que son personnage survive. Le jeu se termine lorsque le personnage du joueur meurt. Dans ta première réponse, demande-lui son nom. Dans la deuxième réponse, demande-lui sa classe (chevalier, mage, archer, etc.). Dans la troisième réponse, demande-lui sa race (humain, elfe, nain, etc.). L'histoire se déroule dans un monde fantastique où un dragon terrifiant règne au sommet d'un volcan. Le but ultime du héros est de vaincre ce dragon. Prépare-toi à improviser de nombreux défis pour guider le joueur jusqu'au dragon. Une fois que le joueur a choisi sa classe et sa race, tu pourras commencer l'aventure en fonction de ses choix. Lorsque tu présenteras les choix, indique clairement les numéros correspondants à chaque option et demande au joueur. N'hésite pas à prendre en compte ses réponses pour créer une expérience immersive et captivante. Si le joueur décide de mettre fin à l'aventure, tu dois absulument répondre `Fin de l'aventure !`.",
-  });
-
   await msg.channel.sendTyping();
 
   let previousMessage = await msg.channel.messages.fetch();
+  previousMessage.reverse();
 
   if (msg.content.startsWith("Jouer")) {
     // supprimer tout les messages sauf celui envoyé par l'utilisateur
@@ -47,7 +54,7 @@ client.on("messageCreate", async (msg) => {
 
     msg.channel.send("Nouvelle Aventure !");
   }
-  previousMessage.forEach((message) => {
+  await previousMessage.forEach((message) => {
     if (message.author.bot && message.author.id !== client.user.id) return;
     if (message.content.startsWith("!")) return;
     if (message.author.id !== msg.author.id) return;
@@ -64,8 +71,11 @@ client.on("messageCreate", async (msg) => {
 
   conversationLog.push({
     role: "system",
-    content: result.choices[0].message,
+    content: result.choices[0].message.content,
   });
+
+  console.log("MESSAGE");
+  console.log(conversationLog);
 
   msg.reply(result.choices[0].message);
 
